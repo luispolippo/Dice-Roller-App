63c2ce91a0ca5e16b88f6c92e007daf3ce7a5769
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-04-02 21:28:51 -0300

    improved compatibility and functionality

--- app/src/main/java/com/polippo/diceroller/MainActivity.kt
+++ app/src/main/java/com/polippo/diceroller/MainActivity.kt
@@ -10,9 +10,14 @@ import kotlin.random.Random
 import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
@@ -23,9 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun diceRoller() {
 
-        val randomInt = java.util.Random().nextInt(6) + 1
-
-        val drawbleResource = when(randomInt){
+        val drawbleResource = when(java.util.Random().nextInt(6) + 1){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -35,7 +38,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.empty_dice
         }
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawbleResource)
     }
 }
